061998682e9e0bb5dee0ab04d57ec36a9f461c13
Ali Davood <alidavood.it@gmail.com>
2021-11-14 18:57:55 +0330

    remove core/view

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path, reverse_lazy
-from django.views.generic import RedirectView
-
-from .views import TermsAndConditionsView
+from django.views.generic import RedirectView, TemplateView
 
 app_name = 'core'
 
 urlpatterns = [
     path('', RedirectView.as_view(url=reverse_lazy('game:statistics'), permanent=False), name='home'),
-    path('terms-and-conditions/', TermsAndConditionsView.as_view(), name='terms-and-conditions'),
+    path('terms-and-conditions/', TemplateView.as_view(template_name="core/terms_and_conditions.html"),
+         name='terms-and-conditions'),
 ]
--- apps/core/views.py
+++
@@ -1,5 +0,0 @@
-from django.views.generic import TemplateView
-
-
-class TermsAndConditionsView(TemplateView):
-    template_name = 'core/terms_and_conditions.html'
